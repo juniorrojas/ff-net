88427d1453ed0a6cdd8d2d9d0a171fe3303e987d
Junior Rojas <jrojasdavalos@gmail.com>
2020-08-15 01:47:08 -0600

    Fix indentation

--- js/common/svg.js
+++ js/common/svg.js
@@ -1,7 +1,7 @@
 const svg = {};
 
 svg.createElement = function(element) {
-	return document.createElementNS("http://www.w3.org/2000/svg", element);
+  return document.createElementNS("http://www.w3.org/2000/svg", element);
 }
 
 module.exports = svg;
