f99fa928fd726e03308a51b90ead453efcc6dcd2
Junior Rojas <jrojasdavalos@gmail.com>
2020-10-13 18:10:17 -0600

    Refactor UI

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ class App {
     
     const container = document.createElement("div");
     container.className = "content-container";
-    document.body.appendChild(container);
+    this.domElement = container;
     
     let row;
     
@@ -94,5 +94,22 @@ class App {
   }
 }
 
-const data = require("./data");
-window.app = new App(data);
+ui.init(() => {
+  const divTitle = document.createElement("div");
+  document.body.appendChild(divTitle);
+  divTitle.className = "title-container";
+  divTitle.textContent = "";
+
+  const h1 = document.createElement("h1");
+  h1.textContent = "ff-net";
+  divTitle.appendChild(h1);
+
+  const h2 = document.createElement("h2");
+  h2.textContent = "feedforward neural network learning in real time";
+  divTitle.appendChild(h2);
+
+  const data = require("./data");
+  const app = new App(data);
+  document.body.appendChild(app.domElement);
+  window.app = app;
+});
--- js/ui/index.js
+++ js/ui/index.js
@@ -1,4 +1,19 @@
+function init(onReady) {
+  const link = document.createElement("link");
+  link.onload = onReady;
+  document.head.appendChild(link);
+  link.rel = "stylesheet";
+  link.type = "text/css";
+  link.href = "css/main.css";
+
+  const mm = document.createElement("meta");
+  mm.name = "monetization";
+  mm.content = "$ilp.uphold.com/aBBxeDFZNa3N";
+  document.head.appendChild(mm);
+}
+
 module.exports = {
+  init: init,
   ControlPanel: require("./ControlPanel"),
   DataCanvas: require("./DataCanvas")
 };
