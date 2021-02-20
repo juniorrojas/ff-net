2396a568db33ba6791a602b5ee860869c1ff2868
Junior Rojas <jrojasdavalos@gmail.com>
2021-02-19 18:34:59 -0700

    Refactor

--- js/ui/index.js
+++ js/ui/index.js
@@ -1,19 +1,12 @@
-function init(onReady) {
-  const link = document.createElement("link");
-  link.onload = onReady;
-  document.head.appendChild(link);
-  link.rel = "stylesheet";
-  link.type = "text/css";
-  link.href = "css/main.css";
-
-  const mm = document.createElement("meta");
-  mm.name = "monetization";
-  mm.content = "$ilp.uphold.com/aBBxeDFZNa3N";
-  document.head.appendChild(mm);
-}
-
 module.exports = {
-  init: init,
+  init: (onReady) => {
+    const link = document.createElement("link");
+    link.onload = onReady;
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href = "css/main.css";
+    document.head.appendChild(link);
+  },
   ControlPanel: require("./ControlPanel"),
   DataCanvas: require("./DataCanvas")
 };
