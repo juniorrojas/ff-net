24abe874560b061d7d6c5b24a360e002f14f39b7
Junior Rojas <jrojasdavalos@gmail.com>
2021-02-19 18:36:23 -0700

    Refactor

--- js/ui/index.js
+++ js/ui/index.js
@@ -1,12 +1,14 @@
+function init(onReady) {
+  const link = document.createElement("link");
+  link.onload = onReady;
+  link.rel = "stylesheet";
+  link.type = "text/css";
+  link.href = "css/main.css";
+  document.head.appendChild(link);
+}
+
 module.exports = {
-  init: (onReady) => {
-    const link = document.createElement("link");
-    link.onload = onReady;
-    link.rel = "stylesheet";
-    link.type = "text/css";
-    link.href = "css/main.css";
-    document.head.appendChild(link);
-  },
+  init: init,
   ControlPanel: require("./ControlPanel"),
   DataCanvas: require("./DataCanvas")
 };
