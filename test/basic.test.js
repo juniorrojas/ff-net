c6c875a4e1c08d045b0b95b78f8ad1cd67b16ef0
Junior Rojas <jrojasdavalos@gmail.com>
2023-03-13 20:27:04 -0500

    Update test

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,12 +1,24 @@
 const ffnet = require("ff-net");
 
-test("test", () => {
+test("single neuron", () => {
   const model = new ffnet.Sequential({
     headless: true
   });
   expect(model.numLayers()).toBe(0);
-  model.addNeuronGroup(2);
+  model.addNeuronGroup(1);
   expect(model.numLayers()).toBe(0);
-  model.addFullyConnectedLayer(2);
+  model.addFullyConnectedLayer(1);
   expect(model.numLayers()).toBe(1);
+
+  const inputNeuronGroup = model.getInputNeuronGroup();
+  const inputNeuron = inputNeuronGroup.neurons[0];
+  inputNeuron.activation = 0;
+  const outputNeuronGroup = model.getOutputNeuronGroup();
+  const outputNeuron = outputNeuronGroup.neurons[0];
+  outputNeuron.bias = 0;
+
+  model.forward();
+  
+  // sigmoid(0) = 0.5
+  expect(outputNeuron.activation).toEqual(0.5);
 });
