551f00e9fe66797c7484a6e122a2140180bb3435
Junior Rojas <jrojasdavalos@gmail.com>
2023-03-14 16:58:30 -0500

    Update test

--- test/backward.test.js
+++ test/backward.test.js
@@ -11,30 +11,32 @@ test("backward", () => {
   expect(model.numLayers()).toBe(1);
   expect(model.numLinks()).toBe(1);
 
+  const inputNeuronGroup = model.getInputNeuronGroup();
+  const inputNeuron = inputNeuronGroup.neurons[0];
+  const outputNeuronGroup = model.getOutputNeuronGroup();
+  const outputNeuron = outputNeuronGroup.neurons[0];
+  const link = model.links[0];
+
   // f(x) = sigmoid(x * w + b)
+
   const x = 0.0;
   const w = 1.0;
   const b = 0.0;
 
-  const inputNeuronGroup = model.getInputNeuronGroup();
-  const inputNeuron = inputNeuronGroup.neurons[0];
   inputNeuron.activation = x;
-  const outputNeuronGroup = model.getOutputNeuronGroup();
-  const outputNeuron = outputNeuronGroup.neurons[0];
   outputNeuron.bias = b;
-  const link = model.links[0];
   link.weight = w;
 
+  // forward
   model.forward();
   
   expect(outputNeuron.activation).toBe(0.5);
 
+  // backward
   outputNeuron.dActivation = 1;
-  expect(outputNeuron.dPreActivation).toBe(0);
-
   model.backward(0, 0);
+
   expect(outputNeuron.dPreActivation).toBe(0.25);
-  
   expect(link.dWeight).toBe(0);
   expect(outputNeuron.dBias).toBe(0.25);
 });
