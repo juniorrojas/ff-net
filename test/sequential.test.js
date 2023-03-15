24cc13a509908f875a5fffdcf641f892b78be663
Junior Rojas <jrojasdavalos@gmail.com>
2023-03-15 00:12:59 -0500

    Update test

--- test/sequential.test.js
+++ test/sequential.test.js
@@ -4,14 +4,32 @@ test("add layers", () => {
   const model = new ffnet.Sequential({
     headless: true
   });
-  expect(model.numNeurons()).toBe(0);
+  expect(model.numNeuronGroups()).toBe(0);
   expect(model.numLayers()).toBe(0);
+  expect(model.numNeurons()).toBe(0);
+  expect(model.numLinks()).toBe(0);
 
   model.addNeuronGroup(2);
-  expect(model.numNeurons()).toBe(2);
+  expect(model.numNeuronGroups()).toBe(1);
   expect(model.numLayers()).toBe(0);
+  expect(model.numNeurons()).toBe(2);
+  expect(model.numLinks()).toBe(0);
 
   model.addFullyConnectedLayer(3);
-  expect(model.numNeurons()).toBe(5);
+  expect(model.numNeuronGroups()).toBe(2);
   expect(model.numLayers()).toBe(1);
+  expect(model.numNeurons()).toBe(5);
+  expect(model.numLinks()).toBe(6);
+
+  model.addFullyConnectedLayer(5);
+  expect(model.numNeuronGroups()).toBe(3);
+  expect(model.numLayers()).toBe(2);
+  expect(model.numNeurons()).toBe(10);
+  expect(model.numLinks()).toBe(21);
+
+  model.clear();
+  expect(model.numNeuronGroups()).toBe(0);
+  expect(model.numLayers()).toBe(0);
+  expect(model.numNeurons()).toBe(0);
+  expect(model.numLinks()).toBe(0);
 });
