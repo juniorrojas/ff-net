d505bdc5aa0aa12332b1773cd9564f6ff8b402c4
Junior Rojas <jrojasdavalos@gmail.com>
2023-03-16 14:49:05 -0500

    Improve error handling

--- ff-net/nn/Layer.js
+++ ff-net/nn/Layer.js
@@ -1,5 +1,11 @@
 class Layer {
   constructor(args = {}) {
+    if (args.inputNeuronGroup == null) {
+      throw new Error("inputNeuronGroup required to create layer");
+    }
+    if (args.outputNeuronGroup == null) {
+      throw new Error("outputNeuronGroup required to create layer");
+    }
     this.inputNeuronGroup = args.inputNeuronGroup;
     this.outputNeuronGroup = args.outputNeuronGroup;
   }
