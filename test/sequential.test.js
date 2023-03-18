aab705842a5eb28f7111f14381e03d1dfacfc7c7
Junior Rojas <jrojasdavalos@gmail.com>
2023-03-18 03:14:40 -0500

    Update test

--- test/sequential.test.js
+++ test/sequential.test.js
@@ -15,11 +15,12 @@ test("add layers", () => {
   expect(model.numNeurons()).toBe(2);
   expect(model.numLinks()).toBe(0);
   expect(model.getInputNeuronGroup()).toBe(g0);
-  expect(model.getOutputNeuronGroup()).toBe(g0);
+  expect(model.getOutputNeuronGroup()).toBe(g0);  
 
   expect(() => { model.addFullyConnectedLayer(); }).toThrow();
 
   const layer0 = model.addFullyConnectedLayer(3);
+  expect(model.layers[0]).toBe(layer0);
   expect(model.numNeuronGroups()).toBe(2);
   expect(model.numLayers()).toBe(1);
   expect(model.numNeurons()).toBe(5);
@@ -28,7 +29,8 @@ test("add layers", () => {
   expect(layer0.inputNeuronGroup).toBe(g0);
   expect(layer0.outputNeuronGroup).toBe(model.getOutputNeuronGroup());
 
-  model.addFullyConnectedLayer(5);
+  const layer1 = model.addFullyConnectedLayer(5);
+  expect(model.layers[1]).toBe(layer1);
   expect(model.numNeuronGroups()).toBe(3);
   expect(model.numLayers()).toBe(2);
   expect(model.numNeurons()).toBe(10);
