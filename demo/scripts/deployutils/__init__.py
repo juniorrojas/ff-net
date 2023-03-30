03ba7d3f6fc156190e29092ac32d2aff19e1e56c
Junior Rojas <jrojasdavalos@gmail.com>
2023-03-30 00:24:31 -0500

    Move more stuff to deployutils

--- demo/scripts/deployutils/__init__.py
+++ demo/scripts/deployutils/__init__.py
@@ -3,6 +3,21 @@ import os
 this_filepath = Path(os.path.abspath(__file__))
 this_dirpath = this_filepath.parent
 import configparser
+import sys
+
+class TextColor:
+    green = "\033[92m"
+    yellow = "\033[93m"
+    end = "\033[0m"
+
+def color_text(color, text):
+    return f"{color}{text}{TextColor.end}"
+
+def yellow(text):
+    return color_text(TextColor.yellow, text)
+
+def green(text):
+    return color_text(TextColor.green, text)
 
 def infer_repo_dirname():
     current_dirpath = Path(os.path.abspath(os.getcwd()))
@@ -11,3 +26,30 @@ def infer_repo_dirname():
             raise RuntimeError("could not infer repo name")
         current_dirpath = current_dirpath.parent
     return str(current_dirpath)
+
+def infer_remote_url():
+    config = configparser.ConfigParser()
+    repo_dirname = infer_repo_dirname()
+    config.read(os.path.join(repo_dirname, ".git", "config"))
+    sections = config.sections()
+    # TODO handle case when .git/config contains multiple remote sections
+    for section in sections:
+        is_remote = False
+        try:
+            section.index("remote")
+            is_remote = True
+        except:
+            pass
+        if is_remote:
+            is_github_remote = False
+            remote_url = config.get(section, "url")
+            print(yellow(f"Inferred remote: {remote_url}"), file=sys.stderr)
+            try:
+                remote_url.index("github")
+                is_github_remote = True
+            except:
+                pass
+            if is_github_remote:
+                return remote_url
+
+    return None
