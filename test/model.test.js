51f9bd5d177a6aeb2074b14fd3cf6f59caa81ff0
Junior Rojas <jrojasdavalos@gmail.com>
2023-04-02 06:16:50 -0500

    Update test

--- test/model.test.js
+++ test/model.test.js
@@ -11,7 +11,7 @@ function expectCloseArrays(a, b) {
   }
 }
 
-test("model", () => {
+test("data loss", () => {
   const model = new ffnet.Sequential({
     headless: true
   });
@@ -61,7 +61,67 @@ test("model", () => {
   const l2bGrad = [0.2270];
 
   outputNeuron.activationGrad = 1.0;
-  model.backward({ regularization: reg });
+  model.backward();
+  expectCloseArrays(l1.getWeightGradArray(), l1wGrad);
+  expectCloseArrays(l1.getBiasGradArray(), l1bGrad);
+  expectCloseArrays(l2.getWeightGradArray(), l2wGrad);
+  expectCloseArrays(l2.getBiasGradArray(), l2bGrad);
+});
+
+test("regularization loss", () => {
+  const model = new ffnet.Sequential({
+    headless: true
+  });
+  model.addNeuronGroup(2);
+  const l1 = model.addFullyConnectedLayer(3);
+  const l2 = model.addFullyConnectedLayer(1);
+
+  const l1w = [
+    [-0.0053,  0.3793],
+    [-0.5820, -0.5204],
+    [-0.2723,  0.1896]
+  ];
+  const l1b = [-0.0140,  0.5607, -0.0628];
+  const l2w = [
+    [ 0.1528, -0.1745, -0.1135]
+  ];
+  const l2b = [-0.5516];
+
+  l1.setWeightFromArray(l1w);
+  expect(l1.getWeightArray()).toEqual(l1w);
+  l1.setBiasFromArray(l1b);
+  expect(l1.getBiasArray()).toEqual(l1b);
+
+  l2.setWeightFromArray(l2w);
+  expect(l2.getWeightArray()).toEqual(l2w);
+  l2.setBiasFromArray(l2b);
+  expect(l2.getBiasArray()).toEqual(l2b);
+
+  const reg = 2.0;
+  const inputNeuronGroup = model.getInputNeuronGroup();
+  inputNeuronGroup.neurons[0].activation = 0.2;
+  inputNeuronGroup.neurons[1].activation = 0.3;
+  model.forward();
+  const regularizationLoss = model.forwardRegularization({ regularization: reg });
+  expect(regularizationLoss).toBeCloseTo(0.9302);
+  const outputNeuron =  model.getOutputNeuronGroup().neurons[0];
+  const output = outputNeuron.activation;
+  expect(output).toBeCloseTo(0.3484);
+
+  const l1wGrad = [
+    [-0.0089,  0.7612],
+    [-1.1659, -1.0437],
+    [-0.5460,  0.3773]
+  ];
+  const l1bGrad = [0.0086, -0.0097, -0.0064];
+  const l2wGrad = [
+    [0.4247, -0.2192, -0.1169]
+  ];
+  const l2bGrad = [0.2270];
+
+  outputNeuron.activationGrad = 1.0;
+  model.backward();
+  model.backwardRegularization({ regularization: reg });
   expectCloseArrays(l1.getWeightGradArray(), l1wGrad);
   expectCloseArrays(l1.getBiasGradArray(), l1bGrad);
   expectCloseArrays(l2.getWeightGradArray(), l2wGrad);
--- test/reg.py
+++ test/reg.py
@@ -9,6 +9,9 @@ model = nn.Sequential(
     nn.Sigmoid()
 )
 
+def reg_loss(r):
+    return 0.5 * r * model[0].weight.pow(2).sum() + model[2].weight.pow(2).sum()
+
 print("w1")
 print(model[0].weight)
 print("b1")
@@ -19,13 +22,21 @@ print("b2")
 print(model[2].bias)
 
 y = model(torch.tensor([[0.2, 0.3]]))
+print("y")
 print(y)
-# y.backward()
-# print("w1")
-# print(model[0].weight.grad)
-# print("b1")
-# print(model[0].bias.grad)
-# print("w2")
-# print(model[2].weight.grad)
-# print("b2")
-# print(model[2].bias.grad)
+
+r = reg_loss(2.0)
+print("r")
+print(r)
+
+loss = (y + reg_loss(2.0))
+loss.backward()
+
+print("w1")
+print(model[0].weight.grad)
+print("b1")
+print(model[0].bias.grad)
+print("w2")
+print(model[2].weight.grad)
+print("b2")
+print(model[2].bias.grad)
