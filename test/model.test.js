8604b7a66965cea7f3aff6f957a329ba55297f36
Junior Rojas <jrojasdavalos@gmail.com>
2023-04-03 03:43:18 -0500

    WIP

--- test/model.test.js
+++ test/model.test.js
@@ -1,41 +1,5 @@
 const ffnet = require("ff-net");
-
-function closeArraysCheck(a, b) {
-  if ((typeof a == "number") && (typeof b == "number")) {
-    if (Math.abs(a - b) <= 1e-3) {
-      return {
-        pass: true,
-      };
-    }
-    return {
-      pass: false,
-      message: () => `not close enough, ${a} != ${b}`
-    };
-  }
-
-  if (!Array.isArray(a) || !Array.isArray(b)) {
-    return {
-      pass: false,
-      message: () => `expected two arrays, found ${typeof a} and ${typeof b}`
-    };
-  }
-
-  if (a.length != b.length) {
-    return {
-      pass: false,
-      message: () => `length mismatch, ${a.length} != ${b.length}`
-    };
-  }
-
-  for (let i = 0; i < a.length; i++) {
-    const r = closeArraysCheck(a[i], b[i]);
-    if (!r.pass) return r;
-  }
-
-  return {
-    pass: true
-  };
-}
+const { closeArraysCheck } = require("./utils");
 
 expect.extend({
   toBeCloseToArray: closeArraysCheck
