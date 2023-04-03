8c2f77bbcbbd53c3eeebc1587347dc9ce9290dd3
Junior Rojas <jrojasdavalos@gmail.com>
2023-04-03 16:54:38 -0500

    WIP

--- test/forward.py
+++ test/forward.py
@@ -9,6 +9,16 @@ model = nn.Sequential(
     nn.Sigmoid()
 )
 
+print("l1w")
+print(model[0].weight)
+print("l1b")
+print(model[0].bias)
+print("l2w")
+print(model[2].weight)
+print("l2b")
+print(model[2].bias)
+
 x = torch.tensor([0.2, 0.3])
 y = model.forward(x)
+print("output")
 print(y)
--- test/forward.test.js
+++ test/forward.test.js
@@ -5,8 +5,8 @@ expect.extend({ toBeCloseToArray });
 test("forward", () => {
   const model = new ffnet.Sequential();
   model.addNeuronGroup(2);
-  const l0 = model.addFullyConnectedLayer(3);
-  const l1 = model.addFullyConnectedLayer(2);
+  const l1 = model.addFullyConnectedLayer(3);
+  const l2 = model.addFullyConnectedLayer(2);
   const y = model.forward([0.2, 0.3]);
   expect(y).not.toBeUndefined();
   expect(y).toBeCloseToArray([0.4867, 0.4025]);
