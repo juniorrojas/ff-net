fc8a2c5b26d2d8cf80349c98828ed1c001509ddf
Junior Rojas <jrojasdavalos@gmail.com>
2023-04-15 08:58:12 -0500

    Build UMD

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,29 @@
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 
-export default {
-  input: "ff-net/index.js",
-  output: {
-    file: "build/ff-net.module.js",
-    format: "esm",
-    sourcemap: false,
+export default [
+  {
+    input: "ff-net/index.js",
+    output: {
+      file: "build/ff-net.module.js",
+      format: "esm",
+      sourcemap: false,
+    },
+    plugins: [
+      resolve(),
+      commonjs()
+    ],
   },
-  plugins: [
-    resolve(),
-    commonjs()
-  ],
-};
+  {
+    input: "ff-net/index.js",
+    output: {
+      file: "build/ff-net.umd.js",
+      format: "umd",
+      name: "ffnet"
+    },
+    plugins: [
+      resolve(),
+      commonjs()
+    ],
+  }
+];
