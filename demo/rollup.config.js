6d018970117e1fb8090330f937ee1e2eba022e3a
Junior Rojas <jrojasdavalos@gmail.com>
2023-04-15 02:47:33 -0500

    WIP

--- demo/rollup.config.js
+++ demo/rollup.config.js
@@ -3,10 +3,14 @@ import resolve from "@rollup/plugin-node-resolve";
 import babel from "rollup-plugin-babel";
 
 export default {
-  input: "js/main.js",
+  input: ["js/main.js"],
   output: {
     file: "js.build.out/main.js",
-    format: "iife"
+    // format: "es"
+    format: "iife",
+    // globals: {
+    //   "ff-net": "ffnet"
+    // }
   },
   plugins: [
     resolve(),
