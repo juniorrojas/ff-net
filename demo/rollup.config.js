8b95c180836963b7aeec73e96bdf19312faa0185
Junior Rojas <jrojasdavalos@gmail.com>
2023-04-15 02:50:56 -0500

    WIP

--- demo/rollup.config.js
+++ demo/rollup.config.js
@@ -1,31 +1,14 @@
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
-import babel from "rollup-plugin-babel";
 
 export default {
   input: ["js/main.js"],
   output: {
     file: "js.build.out/main.js",
-    // format: "es"
     format: "iife",
-    // globals: {
-    //   "ff-net": "ffnet"
-    // }
   },
   plugins: [
     resolve(),
-    commonjs(),
-    babel({
-      presets: [
-        [
-          "@babel/preset-env",
-          {
-            targets: {
-              node: "current",
-            },
-          },
-        ],
-      ],
-    }),
+    commonjs()
   ],
 };
