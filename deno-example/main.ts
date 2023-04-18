ce3eb47890c623c43bd71655fd77c2911ba0205c
Junior Rojas <jrojasdavalos@gmail.com>
2023-04-18 14:05:03 -0500

    Implement simple deno example

---
+++ deno-example/main.ts
@@ -0,0 +1,7 @@
+import ffnet from "https://cdn.rawgit.com/juniorrojas/ff-net/master/build/ff-net.mjs"
+
+const model = new ffnet.Sequential();
+model.addNeuronGroup(3);
+model.addFullyConnectedLayer(4);
+model.addFullyConnectedLayer(1);
+console.log(model.numNeurons());
