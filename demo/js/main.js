5e89360501dbddfc8c4f956f9a8d0834023a250a
Junior Rojas <jrojasdavalos@gmail.com>
2023-04-18 08:13:56 -0500

    WIP

--- demo/js/main.js
+++ demo/js/main.js
@@ -5,6 +5,20 @@ function main() {
   const divTitle = document.createElement("div");
   document.body.appendChild(divTitle);
   divTitle.className = "title-container";
+  (style => {
+    style.backgroundColor = "rgb(46, 53, 56)";
+    style.paddingTop = "25px";
+    style.paddingBottom = "5px";
+    style.paddingRight = "5px";
+    style.paddingLeft = "5px";
+    style.marginBottom = "30px";
+    style.boxShadow = "0px 1px 10px rgba(0, 0, 0, 0.65)";
+  })(divTitle.style);
+
+  document.body.appendChild(divTitle);
+  (style => {
+    style.backgroundColor = "rgb(46, 53, 56)"; 
+  })(divTitle.style);
 
   const h1 = document.createElement("h1");
   h1.textContent = "ff-net";
